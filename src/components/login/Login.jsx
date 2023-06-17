9c9c985e4bb808097dc7d5165bea5f98a851b829
mostofa khan <mdmostofakhan1972@gmail.com>
2023-06-17 13:55:51 -0700

    firebase all data saved

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,5 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
@@ -10,9 +10,10 @@ const auth = getAuth(app);
 const Login = () => {
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
+    const emailRef = useRef()
 
     const handleSubmit = event => {
-        event.preventDefault()
+        event.preventDefault();
         setError('');
         setSuccess('');
         const email = event. target.email.value
@@ -35,7 +36,10 @@ const Login = () => {
         signInWithEmailAndPassword(auth, email, password)
         .then(result =>{
           const loggedUser = result.user;
-          console.log(loggedUser)
+         console.log(loggedUser)
+         if(loggedUser.emailVerified){
+            
+         }
           setSuccess('login in success')
           setError('')
         })
@@ -44,7 +48,23 @@ const Login = () => {
         })
 
     }
+   
+    const handlePasswordEmail = event => {
+        const email = emailRef.current.value
+        if(!email){
+            alert('Please provide your email address to reset password.')
+            return;
+        }
 
+        sendPasswordResetEmail(auth, email)
+        .then(() => {
+            alert('Please cheak your email')
+        })
+        .catch(error => {
+           console.log(error)
+           setError(error.message)
+        })
+    }
    
     return (
         <div className='w-50 mx-auto'>
@@ -52,7 +72,7 @@ const Login = () => {
     <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" name='email' placeholder="Enter email" required/>
+                <Form.Control type="email" name='email' ref={emailRef} placeholder="Enter email" required/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -66,6 +86,7 @@ const Login = () => {
                 Submit
             </Button>
     </Form>
+    <p><small>Forget Password? Please<button onClick={handlePasswordEmail} className='btn btn-link'> Resent password</button></small></p>
     <p><small>New to this website? Please <Link to="/register">Register</Link></small></p>
       <p className='text-danger'>{error}</p>
       <p className='text-success'>{success}</p>
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
+import {createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile} from 'firebase/auth';
 import app from '../firebase.info';
+import { Link } from 'react-router-dom';
 
 
 
@@ -17,7 +18,10 @@ const Register = () => {
        
         const email = event.target.email.value;
         const password = event.target. password.value
-        console.log(email, password)
+        const name = event.target.name.value;
+        console.log(email, password, name)
+
+        
         if(!/(?=.*[A-Z])/.test(password)){
             setError('Please add least one uppercase')
             return;
@@ -31,7 +35,6 @@ const Register = () => {
             return;
         }
 
-
         // creat user in fb;
         createUserWithEmailAndPassword(auth, email, password)
         .then( result => {
@@ -40,6 +43,8 @@ const Register = () => {
             setError('');
             event.target.reset()
            setSuccess('use has been created successfully')
+           sendVerificationEmail(result.user)
+           updateUserData(result.user.name)
         })
         .catch(error => {
             console.log(error.message)
@@ -47,7 +52,28 @@ const Register = () => {
         })
     }
 
-   const handleEmailChangs = (event) => {
+    const sendVerificationEmail = (user) => {
+            sendEmailVerification(user)
+            .then(result => {
+                console.log(result)
+                alert('Please verify your email address')
+            })
+    }
+
+    const updateUserData = (user, name) => {
+        updateProfile(user, {
+            displayName: name
+        })
+        .then( () => {
+            console.log("update profile name")
+        })
+        .catch(error => {
+            setError(error. message)
+        })
+    }
+
+
+   const handleEmailChange = (event) => {
       console.log(event.target.value)
     //   setEmail(event.target.value)
    }
@@ -60,7 +86,10 @@ const Register = () => {
         <div className='w-50 mx-auto'>
             <h4>Please register</h4>
             <form onSubmit={handleSubmit}>
-                <input onChange={handleEmailChangs} className='w-50 mb-4' type="email" name="email" 
+                <input className='w-50 mb-4' type="text" name="name" 
+                 id="name" placeholder='Your Name' required />
+                <br />
+                <input onChange={handleEmailChange} className='w-50 mb-4' type="email" name="email" 
                  id="email" placeholder='Your Email' required />
                 <br />
                 <input onBlur={handlePasswordBlur} className='w-50 mb-4' type="password" name="password"  
@@ -69,6 +98,7 @@ const Register = () => {
                
                 <input className='btn btn-primary' type="submit" value="Register" />
             </form>
+            <p><small>Already have an account? <Link to="/login">Login</Link></small></p>
             <p className='text-danger'>{error}</p>
             <p className='text-primary'>{success}</p>
         </div>
